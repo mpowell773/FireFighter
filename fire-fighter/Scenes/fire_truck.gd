a22d716003542a265ac46915985ccae7c3d495ff
Matt Powell <mpowell171@gmail.com>
2025-12-04 21:02:31 -0500

    Add rotation to driving and adjust values so car drives in proper direction

--- fire-fighter/Scenes/fire_truck.gd
+++ fire-fighter/Scenes/fire_truck.gd
@@ -17,6 +17,7 @@ var steering := 18.0
 var turn_speed := 4.0
 # Below this speed value, car won't turn
 var turn_stop_limit := 0.75
+var set_z_movement := -1.0
 
 var speed_input := 0.0
 var turn_input := 0.0
@@ -24,15 +25,29 @@ var turn_input := 0.0
 
 func _physics_process(_delta: float) -> void:
 	car_mesh.position = position + sphere_offset
+	
 	if ground_ray.is_colliding():
-		apply_central_force(car_mesh.global_transform.basis.z * speed_input)
+		apply_central_force(-car_mesh.global_transform.basis.z * speed_input)
 	
 
-func _process(_delta: float) -> void:
+func _process(delta: float) -> void:
 	if not ground_ray.is_colliding():
-		print("in air")
 		return
-	speed_input = Input.get_axis("brake", "accelerate") * acceleration
-	turn_input = Input.get_axis("steer_left", "steer_right") * deg_to_rad(steering)
+	
+	if Input.is_action_just_pressed("accelerate"):
+		set_z_movement = -1.0
+	elif Input.is_action_just_pressed("brake"):
+		set_z_movement = 1.0
+	
+	speed_input = Input.get_axis("brake", "accelerate") * acceleration * -1.0
+	turn_input = Input.get_axis("steer_left", "steer_right") * deg_to_rad(steering) * set_z_movement
 	wheel_right.rotation.y = turn_input
 	wheel_left.rotation.y = turn_input
+	
+	if linear_velocity.length() > turn_stop_limit:
+		var new_basis := car_mesh.global_transform.basis.rotated(car_mesh.global_transform.basis.y, turn_input)
+		car_mesh.global_transform.basis = car_mesh.global_transform.basis.slerp(new_basis, turn_speed * delta)
+		# Repeated rotations will cause distortion due to floating point imprecision.
+		# To counteract this, utilize the method orthonormalized.
+		car_mesh.global_transform = car_mesh.global_transform.orthonormalized()
+	

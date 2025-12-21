0523c28e68d98526cd3536377c7771935559f77c
Matt Powell <mpowell171@gmail.com>
2025-12-21 16:44:15 -0500

    Begin implementing truck tip correction

--- fire-fighter/Scenes/fire_truck.gd
+++ fire-fighter/Scenes/fire_truck.gd
@@ -8,15 +8,25 @@ var turn_amount := 0.3
 var z_movement := 0.0
 var turn_movement := 0.0
 
+var has_corrected_vehicle_tip := false
+
 @onready var cam_arm: SpringArm3D = $CamArm
 @onready var wheel_back_left: VehicleWheel3D = $WheelBackLeft
 @onready var wheel_back_right: VehicleWheel3D = $WheelBackRight
 @onready var wheel_front_left: VehicleWheel3D = $WheelFrontLeft
 @onready var wheel_front_right: VehicleWheel3D = $WheelFrontRight
+@onready var vehicle_tip_timer: Timer = $VehicleTipTimer
 
-
+var wait_frame_counter: int = 0
 func _physics_process(delta: float) -> void:
 	cam_arm.position = position
+	
+	wait_frame_counter += 1
+	if wait_frame_counter >= 60:
+		print(global_rotation_degrees.z)
+		wait_frame_counter = 0
+	
+	correct_vehicle_tip()
 
 	var RPM_left := absf(wheel_back_left.get_rpm())
 	var RPM_right := absf(wheel_back_right.get_rpm())
@@ -34,3 +44,21 @@ func _physics_process(delta: float) -> void:
 func _process(_delta: float) -> void:
 	z_movement = Input.get_axis("accelerate", "brake") * -1.0
 	turn_movement = Input.get_axis("steer_left", "steer_right") * -1.0
+
+
+func correct_vehicle_tip() -> void:
+	var correction_force := 1500.0
+	if absf(global_rotation_degrees.z) > 60.0 and not has_corrected_vehicle_tip:
+		if signf(global_rotation_degrees.z) == 1:
+			apply_torque_impulse(Vector3(0, 0, 1.0) * correction_force)
+			print("positive impulse")
+		else:
+			apply_torque_impulse(Vector3(0, 0, -1.0) * correction_force)
+			print("negative impulse")
+		has_corrected_vehicle_tip = true
+		vehicle_tip_timer.start()
+
+
+func _on_vehicle_tip_timer_timeout() -> void:
+	has_corrected_vehicle_tip = false
+	print("timed out")
